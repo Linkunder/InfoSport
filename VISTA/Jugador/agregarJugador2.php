4dcc8b737e4e3758bf71de5285a90fb789db93b9
pabloSB27 <pnsilva@alumnos.ubiobio.cl>
2015-11-22 19:00:02 -0300

    Hace lo mismo que el otro
    Hice este porque fue la solucion mas rapida

---
+++ VISTA/Jugador/agregarJugador2.php
@@ -0,0 +1,42 @@
+
+
+<!-- Aqui insertar el jugador al grupo, y que vuelva a la seccion de busqueda -->
+
+<?php
+include_once('../../TO/Jugador.php');
+include_once('../../LOGICA/infoJugadores.php');
+
+include_once('../../TO/GrupoConformado.php');
+include_once('../../LOGICA/infoGruposConformados.php');
+
+
+ob_start();
+$id_grupo2 = $_GET['id_grupo'];
+echo "ID Grupo: $id_grupo2 <br>";
+
+ob_start();
+$idNuevo = $_GET['id_jugador'];
+echo "ID Jugador: $idNuevo <br>";
+
+
+
+
+$nuevoJugadorGrupo= new GrupoConformado();
+$nuevoJugadorGrupo->setIdJugador($idNuevo);
+$nuevoJugadorGrupo->setIdGrupo($id_grupo2);
+
+
+
+$jefeGrupo = infoGruposConformados::obtenerInstancia();
+$vectorGrupo=$jefeGrupo->agregarJugador($nuevoJugadorGrupo);
+
+
+echo "<script type='text/javascript'>alert('Jugador agregado!');</script>";
+header("Location:crearGrupo.php");
+
+
+
+
+?>
+
+
