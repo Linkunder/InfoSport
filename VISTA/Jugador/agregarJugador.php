a347b2a0c2292c203f5998f78e01a36ec3abe6da
pabloSB27 <pnsilva@alumnos.ubiobio.cl>
2015-11-22 19:01:28 -0300

    Agrega un jugador al grupo

--- VISTA/Jugador/agregarJugador.php
+++ VISTA/Jugador/agregarJugador.php
@@ -1,39 +1,46 @@
 
-<?php
-session_start();
-if($_SESSION["autentica"]=="SI") {
 
-}
-else {
-header("Location:login.php"); 
-}
-?>
-<?php include('headerJugador.php');?>
 
 <!-- Aqui insertar el jugador al grupo, y que vuelva a la seccion de busqueda -->
 
+<?php
+include_once('../../TO/Jugador.php');
+include_once('../../LOGICA/infoJugadores.php');
+
+include_once('../../TO/GrupoConformado.php');
+include_once('../../LOGICA/infoGruposConformados.php');
+
+
+ob_start();
+$id_grupo2 = $_GET['id_grupo'];
+echo "ID Grupo: $id_grupo2 <br>";
+
+ob_start();
+$idNuevo = $_GET['id_jugador'];
+echo "ID Jugador: $idNuevo <br>";
 
 
 
 
+$nuevoJugadorGrupo= new GrupoConformado();
+$nuevoJugadorGrupo->setIdJugador($idNuevo);
+$nuevoJugadorGrupo->setIdGrupo($id_grupo2);
 
 
 
+$jefeGrupo = infoGruposConformados::obtenerInstancia();
+$vectorGrupo=$jefeGrupo->agregarJugador($nuevoJugadorGrupo);
 
 
+echo "<script type='text/javascript'>alert('Jugador agregado!');</script>";
+header("Location:grupos.php");
 
 
-
-
-
+?>
 
 
 <br>
 <br>
-
-
-
-
 <?php include('footer.php');?>
     <!-- Footer section end -->
     
