743d069bd0f94641b0032d31720d579c4eaefebb
eternaletulf <eternaletulf@gmail.com>
2015-11-29 12:25:29 -0300

    Correos automaticos al crear cuenta
    RegistrarJugador envia correo con los datos del nuevo perfil

--- VISTA/registrarJugador.php
+++ VISTA/registrarJugador.php
@@ -40,4 +40,60 @@ $jefeJugador->guardarJugador($nuevoJugador);
 echo "<script type='text/javascript'>alert('Jugador agregado!');</script>";
 header("Location:subirImagen.php");
 
+$to = $correo;
+//rellenar con nombre jugador
+
+$subject = "Gracias por registrarte!";
+
+$message = "<html>";
+$message .= "<head>";
+$message .= "<title>HTML email</title>";
+$message .= "</head>";
+$message .= "<body>";
+$message .= '<div style="height:auto; width:auto;"><img src="http://i.imgur.com/fuWkXQT.png" alt="Website Change Request" /></div>';
+$message .= "<p>".$nombre.", ahora eres parte de Infosport, el sistema para encuentros deportivos.</p>";
+$message .= "<p>Los datos que seleccionaste para tu cuenta, son los siguientes: </p>";
+$message .= "<table>";
+$message .= "<tr>";
+$message .= "<td>Nombre completo :</td>";
+$message .= "<td>".$nombre. " ".$apellido. "</td>";
+$message .= "</tr>";
+$message .= "<tr>";
+$message .= "<td>Sexo: :</td>";
+$message .= "<td>".$sexo."</td>";
+$message .= "</tr>";
+$message .= "<tr>";
+$message .= "<td>Fecha de nacimiento:</td>";
+$message .= "<td>".$fecha_nacimiento."</td>";
+$message .= "</tr>";
+$message .= "<tr>";
+$message .= "<td>Posicion preferida: :</td>";
+$message .= "<td>".$posicion."</td>";
+$message .= "</tr>";
+$message .= "<tr>";
+$message .= "<td>Deporte Favorito:</td>";
+$message .= "<td>".$deporte_fav."</td>";
+$message .= "</tr>";
+$message .= "<tr>";
+$message .= "<td>Contraseña :</td>";
+$message .= "<td>".$password."</td>";
+$message .= "</tr>";
+$message .= "</table>";
+$message .= "<center><b><p>© 2015 Ing.SW., Infosport.</p></b></center>";
+$message .= "</body>";
+$message .= "</html>";
+
+
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <infosport2k15@gmail.com>' . "\r\n"; //quien envia, se usa por defecto Infosport2k15@gmail.com
+$headers .= 'Cc: infosport2k15@gmail' . "\r\n"; // se envia con copia a infosport, no es necesario enviarle copia al creador del partido ya que se hace antes.
+
+mail($to,$subject,$message,$headers);
+
+
 ?>
